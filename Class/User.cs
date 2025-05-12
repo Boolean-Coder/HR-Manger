df64cd655044fec0c2fcc72a9841ceea6c78e4af
Boolean-Coder <emre.eo522@gmail.com>
2025-05-12 10:21:23 +0200

    Tag-1 |Erster Comitt: Ich Comitte für mein Leben gern

---
+++ Class/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRManager.Class
+{
+    internal class Class1
+    {
+    }
+}
